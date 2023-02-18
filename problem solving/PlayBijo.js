db395f2ee77f287c6184eea0e145d726da0004a7
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-02-18 10:50:06 +0200

    Play Game

---
+++ problem solving/PlayBijo.js
@@ -0,0 +1,11 @@
+function playGame(string){
+if(string.charAt(0)==="R"|| string.charAt(0)==="r"){
+  return `${string} Gamer is play In GAme`
+}
+else{
+  return `${string} Gamer is play out of GAme`
+
+}
+
+}
+console.log(playGame("Ramy"))
